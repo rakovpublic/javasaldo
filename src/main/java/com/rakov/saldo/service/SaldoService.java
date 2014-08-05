1dbd0ed1765e26a23cbdb939307c8727a35ac232
rakovpublic <rakovpublic@gmail.com>
2014-08-05 15:33:04 +0300

    Added combine split analyze methods
    Added combine split analyze methods to saldo service

--- src/main/java/com/rakov/saldo/service/SaldoService.java
+++ src/main/java/com/rakov/saldo/service/SaldoService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.rakov.saldo.model.Lemgram;
 
 public interface SaldoService {
-	public String split(String word, String pos);
+	public String[] split(String word, String pos);
 	public boolean isSemanticCompound(String word, String segs, String pos); 
 	public HashMap<String, List<Lemgram>> getAncestors(String lemgram);
 }
